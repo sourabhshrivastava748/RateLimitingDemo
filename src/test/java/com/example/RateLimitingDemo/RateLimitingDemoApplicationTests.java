57246c53b7303b3c91ee167dbe597c966f48316e
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2024-05-21 16:07:49 +0530

    Initial commit

---
+++ src/test/java/com/example/RateLimitingDemo/RateLimitingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RateLimitingDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
